7c1457a08c54baaca023fdfd54ea4113c76f7d92
Bilal <solidhydrogenblock@gmail.com>
2024-01-15 19:17:25 -0800

    Test with environment variable

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,9 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('print test environment variable', async ({ page }) => {
+  console.log(process.env.CUSTOM_ENV_VARIABLE);
+
+  expect(process.env.CUSTOM_ENV_VARIABLE).toBe("hello world :)");
+});
